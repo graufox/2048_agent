f53b0dd1beeedbc8c8d115c0aebf01edebeb33d2
Angier Allen <30650080+graufox@users.noreply.github.com>
2019-02-06 19:14:06 -0800

    initial commit, part 1

---
+++ agent_random.py
@@ -0,0 +1,82 @@
+import numpy as np
+from numpy.random import rand, randn, randint
+import matplotlib.pyplot as plt
+
+from game import Game
+from funcs import ema
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Run an agent to play a game of 2048 with random moves.')
+parser.add_argument('--num_episodes', type=int, default=500,
+                    help='Number of episodes to run for.')
+args = parser.parse_args()
+num_episodes = args.num_episodes
+
+
+
+# define environment, in this case a game of 2048
+env = Game()
+
+# make model for Q
+# set hyperparameters
+episode_length = 2**20 # max number of moves per game
+
+
+# set up lists for keeping track of progress
+scores = []
+rewards = []
+observations = []
+
+# iterate through a number of episodes
+for i_episode in range(num_episodes):
+
+    if i_episode % 10 == 0:
+        print('\tepisode {}'.format(i_episode))
+        print('\t\t{}'.format(np.mean(scores[-10:])))
+
+    # start with a fresh environment
+    observation = env.reset()
+
+    # run the simulation
+    episode_reward = 0
+    for t in range(episode_length):
+
+        # if i_episode % 10 == 0:
+        #     print(env.board)
+        #     print('-'*10)
+
+        # choose random action
+        action = [randint(4)]
+
+        # make a step in the environment
+        new_observation, reward, done, info = env.step(action[0])
+        episode_reward += reward
+
+        observation = new_observation
+        observations += [new_observation]
+
+        if done:
+            break
+
+    scores += [env.score]
+    rewards += [episode_reward]
+
+observations = np.stack(observations)
+scores = np.array(scores)
+rewards = np.array(rewards)
+print('\tAverage fitness: {}'.format(np.mean(scores)))
+print('\tStandard Deviation of Fitness: {}'.format(np.std(scores)))
+
+plt.plot(scores)
+plt.plot(ema(scores, 0.1))
+plt.axis([0,num_episodes,0,10000])
+plt.title('Scores Over Time')
+plt.show()
+
+plt.plot(rewards)
+plt.plot(ema(rewards, 0.1))
+# plt.axis([0,num_episodes,-5000,5000])
+plt.title('Fitness Over Time')
+plt.show()
---
+++ funcs.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+
+def ema(data, a):
+    """
+    exponential moving average of `data`
+        data   =  [1-D numpy array] the data to smooth
+        a      =  [float between 0 and 1] the smoothness factor
+    """
+    try:
+        if len(data.shape) == 1:
+            smooth_data = np.zeros(data.shape)
+            smooth_data[0] = data[0]
+            for j in range(1,len(data)):
+                smooth_data[j] = (1-a)*smooth_data[j-1] + a*data[j]
+            return smooth_data
+        else:
+            print('\terror: data must be a 1-D numpy array')
+            return -1
+    except:
+        print('\terror: data must be a 1-D numpy array')
+        return -1
+
+
+
+
+
+def board_2_array(board):
+    """
+    outputs a 4 x 4 x 16 array with one-hot encodings for the tiles
+        board  = 4 x 4 numpy array of tile values associated to board
+    """
+    try:
+        if len(board.shape) == 2:
+            channels = []
+            channels += [np.zeros(shape=(4,4)) == board]
+            for i in range(1,16):
+                channels += [np.ones(shape=(4,4))*(2**i) == board]
+            return np.stack(channels, axis=-1)
+        else:
+            print('\terror: data must be a 2-D numpy array')
+            return -1
+    except:
+        print('\terror: data must be a 2-D numpy array')
+        return -1
+
+
+
+
+
+def sample_action(Qvals, moves):
+    """
+    Samples an action proportional to Qvals and available moves
+    """
+    temp = (Qvals[0] - Qvals[0].min()) * moves[0]
+    if temp.max() > 100:
+        p = temp[0] / temp[0].sum()
+    else:
+        p = np.exp(temp)[0] * moves[0]
+    if p.sum() < 1e-5:
+        p = (p>0)
+    p = p / p.sum()
+    return [np.random.choice([0,1,2,3], p=p)]
