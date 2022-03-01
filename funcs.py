150be2a84c66bb195ce3592281d2f2a3fbf43570
Angier Allen <allen.angier@gmail.com>
2022-02-28 23:27:15 -0800

    update funcs with asserts

--- funcs.py
+++ funcs.py
@@ -7,19 +7,12 @@ def ema(data, a):
         data   =  [1-D numpy array] the data to smooth
         a      =  [float between 0 and 1] the smoothness factor
     """
-    try:
-        if len(data.shape) == 1:
-            smooth_data = np.zeros(data.shape)
-            smooth_data[0] = data[0]
-            for j in range(1, len(data)):
-                smooth_data[j] = (1 - a) * smooth_data[j - 1] + a * data[j]
-            return smooth_data
-        else:
-            print("\terror: data must be a 1-D numpy array")
-            return -1
-    except:
-        print("\terror: data must be a 1-D numpy array")
-        return -1
+    assert len(data.shape) == 1:
+        smooth_data = np.zeros(data.shape)
+        smooth_data[0] = data[0]
+        for j in range(1, len(data)):
+            smooth_data[j] = (1 - a) * smooth_data[j - 1] + a * data[j]
+        return smooth_data
 
 
 def board_2_array(board, board_size=4, board_depth=16):
@@ -27,19 +20,12 @@ def board_2_array(board, board_size=4, board_depth=16):
     outputs a 4 x 4 x 16 array with one-hot encodings for the tiles
         board  = 4 x 4 numpy array of tile values associated to board
     """
-    try:
-        if len(board.shape) == 2:
-            channels = []
-            channels += [np.zeros(shape=(board_size, board_size)) == board]
-            for i in range(1, board_depth):
-                channels += [np.ones(shape=(board_size, board_size)) * (2**i) == board]
-            return np.stack(channels, axis=-1)
-        else:
-            print("\terror: data must be a 2-D numpy array")
-            return -1
-    except:
-        print("\terror: data must be a 2-D numpy array")
-        return -1
+    assert len(board.shape) == 2
+    channels = []
+    channels += [np.zeros(shape=(board_size, board_size)) == board]
+    for i in range(1, board_depth):
+        channels += [np.ones(shape=(board_size, board_size)) * (2**i) == board]
+    return np.stack(channels, axis=-1)
 
 
 def sample_action(Qvals, moves):
