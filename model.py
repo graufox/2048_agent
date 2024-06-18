7819b07e72415ae4b6c397d6b1dfcbf2fd6239d3
Angier Allen <allen.angier@gmail.com>
2024-06-17 23:06:30 -0700

    remove unused class and update gradient clip

--- model.py
+++ model.py
@@ -260,64 +260,7 @@ class ReinforcementAgent(tf.keras.models.Model):
             loss_value = tf.keras.losses.Huber()(targetQ, Q)
             grads = tape.gradient(loss_value, self.trainable_weights)
             grads = [
-                (None if gradient is None else tf.clip_by_norm(gradient, 1.0))
-                for gradient in grads
-            ]
-            self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-        return loss_value
-
-
-class LinearReinforcementAgent(tf.keras.models.Model):
-    """
-    Deep Q-Network Reinforcement Learning Agent
-
-    Deep Q-Network Reinforcement Learning Agent, implemented in TensorFlow 2.
-        The network is based on a deep residual convolution network. The output
-        are one of 4 actions, which are associated with up, right, down, and
-        left respectfully. The output of the network is the estimated Q-value
-        for the actions at the given state.
-    """
-
-    def __init__(
-        self,
-        conv_filters=128,
-        conv_dropout=0.2,
-        num_conv_stacks=3,
-        dense_units=(1024,),
-        output_units=4,
-        dense_dropout=0.5,
-        kernel_size=(3, 3),
-    ):
-        super().__init__()
-
-        self.base_model = ConvModel(
-            conv_filters=conv_filters,
-            conv_dropout=conv_dropout,
-            num_conv_stacks=num_conv_stacks,
-            dense_units=dense_units,
-            dense_dropout=dense_dropout,
-            kernel_size=kernel_size,
-            output_units=output_units,
-            output_activation=None,
-        )
-
-    @tf.function
-    def call(self, inputs, training=False):
-        observation, available_moves = inputs
-        logQ = self.base_model(observation, training=training)
-        Q = tf.nn.softplus(logQ)
-        Q_masked = Q * available_moves
-        action = tf.argmax(Q_masked, axis=1)
-        return Q, action
-
-    @tf.function
-    def train_step(self, x, targetQ):
-        with tf.GradientTape() as tape:
-            Q, _ = self(x, training=True)
-            loss_value = tf.keras.losses.Huber()(targetQ, Q)
-            grads = tape.gradient(loss_value, self.trainable_weights)
-            grads = [
-                (None if gradient is None else tf.clip_by_norm(gradient, 1.0))
+                (None if gradient is None else tf.clip_by_norm(gradient, 10.0))
                 for gradient in grads
             ]
             self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
@@ -395,7 +338,7 @@ class RotationalReinforcementAgent(tf.keras.models.Model):
             loss_value = tf.keras.losses.Huber()(targetQ, Q)
             grads = tape.gradient(loss_value, self.trainable_weights)
             grads = [
-                (None if gradient is None else tf.clip_by_norm(gradient, 0.3))
+                (None if gradient is None else tf.clip_by_norm(gradient, 10.0))
                 for gradient in grads
             ]
             self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
